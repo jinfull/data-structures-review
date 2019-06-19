a8eb11b81fb74bc6c67b53b2bfb0a54237b014a4
jinfull <jinfull@gmail.com>
2019-06-19 16:29:25 -0700

    Complete queue implementation

--- 02-queue_project/lib/queue.js
+++ 02-queue_project/lib/queue.js
@@ -21,11 +21,52 @@
 // -----------
 
 class Node {
-
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
 }
 
 class Queue {
+  constructor() {
+    this.front = null;
+    this.back = null;
+    this.length = 0;
+  }
 
+  enqueue(val) {
+    let newNode = new Node(val);
+
+    if (this.length === 0) {
+      this.front = newNode;
+      this.back = newNode;
+    } else {
+      this.back.next = newNode;
+      this.back = newNode;
+    }
+
+    return ++this.length;
+  }
+
+  dequeue() {
+    let removedNode = this.front;
+
+    if (this.length === 0) {
+      return null;
+    } else if (this.length === 1) {
+      this.front = null;
+      this.back = null;
+    } else {
+      this.front = this.front.next;
+    }
+
+    this.length--;
+    return removedNode.value;
+  }
+
+  size() {
+    return this.length;
+  }
 }
 
 exports.Node = Node;
